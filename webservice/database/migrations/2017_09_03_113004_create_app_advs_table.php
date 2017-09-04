67916ab8e36533aeca24637b39e95fd0677e21d9
Alexis Richard <ecmascript.guru@gmail.com>
2017-09-04 01:01:25 -0400

    appAdvs migration created

---
+++ webservice/database/migrations/2017_09_03_113004_create_app_advs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppAdvsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_advertisementss', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('operating_company_id')->unsigned();
+            $table->foreign('operating_company_id')->references('id')->on('operating_companies');
+            $table->string('title', 10);
+            $table->text('image_url')->nullable();
+            $table->string('image_title', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_advertisementss');
+    }
+}
